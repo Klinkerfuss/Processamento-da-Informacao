f8e28c22c853f854d389836a915f92ff96da08bf
Klinkerfuss <G.Klinkerfuss@hotmail.com>
2021-02-18 21:10:27 -0300

    Create Par.py
    This is a program created to differentiate even and odd numbers

---
+++ Par.py
@@ -0,0 +1,8 @@
+#Lista 0
+#Programa que diferencia numeros pares e impares
+x=int(input('Digite um número: '))
+par=x%2
+if par == 0:
+  print('Esse número é Par.')
+else:
+  print('Esse número é ímpar.')
